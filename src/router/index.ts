13598047b37f6f539535b45dc2ff015a72744425
onetuks <onetuks@naver.com>
2026-07-15 10:30:31 +0900

    chore: router 추가

---
+++ src/router/index.ts
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Overview from '../views/Overview.vue';
+import Landscape from '../views/Landscape.vue';
+import Rulesets from '../views/Rulesets.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/overview'
+    },
+    {
+      path: '/overview',
+      name: 'Overview',
+      component: Overview
+    },
+    {
+      path: '/landscape',
+      name: 'Landscape',
+      component: Landscape
+    },
+    {
+      path: '/rulesets',
+      name: 'Rulesets',
+      component: Rulesets
+    },
+    {
+      path: '/run',
+      name: 'Run',
+      component: () => import('../views/Run.vue')
+    },
+    {
+      path: '/report',
+      name: 'Report',
+      component: () => import('../views/Report.vue')
+    },
+    {
+      path: '/library',
+      name: 'Library',
+      component: () => import('../views/Library.vue')
+    }
+  ]
+});
+
+export default router;
